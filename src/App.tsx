b5beb44a56159a554931e3159f033ae8936519ec
Masahiro Kiyota <hiro@juzbox.com>
2018-05-16 01:12:55 +0900

    added router & detail scaffold

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
+import { Router, Route, Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { Container, Row, Col, NavbarBrand, Navbar, NavbarToggler, Collapse, Nav, NavItem, NavLink, Card, CardImg, CardTitle, CardBody, CardColumns, CardText } from 'reactstrap';
-import { FaClockO, FaDashboard, FaFlagCheckered, FaHourglass2 } from 'react-icons/lib/fa';
+import { FaClockO, FaCutlery, FaDashboard, FaFlagCheckered, FaHourglass2 } from 'react-icons/lib/fa';
 import './App.css';
 import * as recipes from './recipes.json';
+import history from './history';
 
 
 class AppNavbar extends React.Component<{}, {isOpen: boolean}> {
@@ -14,7 +16,7 @@ class AppNavbar extends React.Component<{}, {isOpen: boolean}> {
   public render() {
     return (
       <Navbar color="light" sticky="top" light expand="sm">
-        <NavbarBrand href="/">C4M-E Recipes</NavbarBrand>
+        <NavbarBrand><Link to='/'>C4M-E Recipes</Link></NavbarBrand>
         <NavbarToggler onClick={this.toggle} />
         <Collapse isOpen={this.state.isOpen} navbar>
           <Nav className="ml-auto" navbar>
@@ -31,6 +33,14 @@ class AppNavbar extends React.Component<{}, {isOpen: boolean}> {
   }
 }
 
+const RecipeDetail = (props: RouteComponentProps<{id: number}>) => {
+  const recipeId = props.match.params.id;
+  // TODO: add code to show detail
+  return (
+    <h1>Recipe ID: {recipeId}</h1>
+  );
+}
+
 const RecipeItem = (props: {r: Recipe}) => (
     <Card>
       <CardImg top width="100%" src={'/imgs/'+props.r.id.toString()+'.jpg'}/>
@@ -40,6 +50,7 @@ const RecipeItem = (props: {r: Recipe}) => (
         <CardText>
           <FaClockO />準備時間：{props.r.prep_duration}分<br/>
           <FaHourglass2 />加熱時間：{props.r.cook_duration}分<br/>
+          <FaCutlery/>種類：{props.r.kind}<br/>
           <FaDashboard />難易度：{props.r.difficulty}<br/>
           <FaFlagCheckered/>ジャンル：{props.r.genre}
         </CardText>
@@ -47,19 +58,33 @@ const RecipeItem = (props: {r: Recipe}) => (
     </Card>
 );
 
+const SearchResult = withRouter((props: RouteComponentProps<{}>) => {
+  // TODO: add code to filter using query
+  const q = props.location.search;
+    return (
+    <div>
+    <h1>{props.location.search}</h1>
+    <CardColumns>
+    {recipes.map((recipe) => {
+      return <RecipeItem r={recipe} key={recipe.id} />;
+    })}
+    </CardColumns>
+    </div>
+  );
+});
+
 class App extends React.Component {
   public render() {
     return (
+      <Router history={history}>
       <div>
         <AppNavbar/>
         <Container fluid={true}>
-          <CardColumns>
-          {recipes.map((recipe) => {
-            return <RecipeItem r={recipe} key={recipe.id} />;
-          })}
-          </CardColumns>
+          <Route exact path="/" component={SearchResult}/>
+          <Route path="/detail/:id" component={RecipeDetail}/>
         </Container>
       </div>
+      </Router>
     );
   }
 }
