ccd497ff29055836152a901d0adac3a2e8ae3531
Masahiro Kiyota <hiro@juzbox.com>
2018-05-15 22:51:39 +0900

    enabled typed json import

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,4 +1,41 @@
-declare module "*.json" {
-    const value: any;
-    export default value;
+/*
+// won't use since inverse mapping for string enum is not available
+
+enum Difficulty {
+  Normal = "普通",
+  Easy = "簡単"
+}
+
+enum Genre {
+   Ethnic = "エスニック",
+   French = "フレンチ",
+   Chinese = "中華風",
+   Japanese = "和風",
+   Western = "洋風"
+}
+*/
+
+interface Ingredient {
+    name: string;
+    detail?: string;
+    amount?: string;
+}
+    
+interface Recipe {
+    id: number;
+    title: string;
+    cook_duration: number;
+    calorie: number;
+    genre: string;
+    difficulty: string;
+    prep_duration: number;
+    comment: string;
+    ingredients: Ingredient[]
+    yield: number;
+    instructions: string[]
+}
+
+declare module "*/recipes.json" {
+    const value: Recipe[];
+    export = value;
 }
