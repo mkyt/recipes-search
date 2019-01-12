93206da0d9a96120233e2e3e5f0167991f9ce051
Masahiro Kiyota <hiro@juzbox.com>
2019-01-13 02:10:39 +0900

    Updates

---
+++ precache-manifest.10b8d5373a2d912ffd245f0ea3c78ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7b9d5503252dd036a5b",
+    "url": "/recipes-search/static/js/runtime~main.f7b9d550.js"
+  },
+  {
+    "revision": "88c4bdb6221eab529ecc",
+    "url": "/recipes-search/static/js/main.88c4bdb6.chunk.js"
+  },
+  {
+    "revision": "175c9732efd6d3be2f08",
+    "url": "/recipes-search/static/js/1.175c9732.chunk.js"
+  },
+  {
+    "revision": "88c4bdb6221eab529ecc",
+    "url": "/recipes-search/static/css/main.dd7ecb8e.chunk.css"
+  },
+  {
+    "revision": "175c9732efd6d3be2f08",
+    "url": "/recipes-search/static/css/1.7fdfef8f.chunk.css"
+  },
+  {
+    "revision": "df82dabd49bed7ef23998756d5e329b7",
+    "url": "/recipes-search/index.html"
+  }
+];
